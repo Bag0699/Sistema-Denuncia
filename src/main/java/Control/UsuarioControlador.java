cb0f9f51ad39918179cfde9cc72006766300fc56
Brayam Ayala <b.ayalaga06@gmail.com>
2024-10-15 22:38:22 -0500

    Creación del sistema de incidencias

---
+++ src/main/java/Control/UsuarioControlador.java
@@ -0,0 +1,35 @@
+package Control;
+
+import Modelo.ProcedurePrototype;
+import Modelo.Usuario;
+
+import java.sql.SQLException;
+
+public class UsuarioControlador implements ControladorBase{
+    private final ProcedurePrototype procedurePrototype;
+
+    public UsuarioControlador() {
+        procedurePrototype = new ProcedurePrototype();
+    }
+
+    public void crearUsuario(Usuario usuario) throws  SQLException {
+        procedurePrototype.crearUsuario(usuario);
+    }
+    public Usuario leerUsuario(String idUsuario) throws SQLException {
+        return procedurePrototype.leerUsuario(idUsuario);
+    }
+
+    public void actualizarUsuario(Usuario usuario) throws SQLException {
+        procedurePrototype.actualizarUsuario(usuario);
+    }
+
+    public void eliminarUsuario(String idUsuario) throws SQLException {
+        procedurePrototype.eliminarUsuario(idUsuario);
+    }
+
+    @Override
+    public void ejecutarControlador() {
+        System.out.println("Ejecutando el controlador de Usuario!!");
+    }
+
+}
---
+++ src/main/java/Modelo/ConexionBdSingleton.java
@@ -0,0 +1,48 @@
+package Modelo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexionBdSingleton {
+
+    private static ConexionBdSingleton instance;
+    private Connection cn;
+
+    private String url = "jdbc:sqlserver://localhost:1433;database=DenunciasDb";
+    private String user = "sa";
+    private String psw = "123456";
+
+    public ConexionBdSingleton()throws SQLException{
+        
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            this.cn = DriverManager.getConnection(url, user, psw);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException(e);
+        }
+    }
+    public void cerrarConexion() throws SQLException {
+        try {
+            cn.close();
+        } catch (SQLException e){
+            e.printStackTrace();
+            cn.close();
+        }finally {
+            cn.close();
+        }
+    }
+
+    public static ConexionBdSingleton getInstace() throws SQLException{
+        if(instance == null){
+            instance = new ConexionBdSingleton();
+        }else if (instance.getConnection().isClosed()){
+            instance = new ConexionBdSingleton();
+        }
+        return instance;
+    }
+    
+    public Connection getConnection(){
+        return cn;
+    }
+}
